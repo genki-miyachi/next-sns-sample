6e34d7968e806d2746ee1895dfcb3573fde58c82
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-19 11:10:28 +0900

    Add: install encrypt pricess

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
 
 const PORT = 8000;
 
@@ -10,12 +11,14 @@ const prisma = new PrismaClient();
 app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body;
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await prisma.user.create({
     data: {
       username,
       email,
-      password,
-    },j
+      password: hashedPassword,
+    },
   });
 
   return res.json({ user });
