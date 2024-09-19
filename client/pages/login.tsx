ca25320d7f2db318bebf6efe707bac7d78bfc5dd
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-19 15:41:16 +0900

    Update: Push token to LocalStorage

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "@/context/auth";
 import apiClient from "@/lib/apiClient";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -8,6 +9,7 @@ const Login = () => {
   const [password, setPassword] = useState<string>("");
 
   const router = useRouter();
+  const { login } = useAuth();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,7 +20,9 @@ const Login = () => {
         password,
       });
       const token = response.data.token;
-      console.log(token);
+
+      login(token);
+
       router.push("/");
     } catch (error) {
       alert("入力内容が正しくありません");
