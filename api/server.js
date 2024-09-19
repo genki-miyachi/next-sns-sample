ea418eaa24dc6041e8d4e9a053087b923a84b3e8
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-19 11:04:51 +0900

    Update server

--- api/server.js
+++ api/server.js
@@ -1,6 +1,24 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const { PrismaClient } = require("@prisma/client");
 
-const PORT = 8000
+const PORT = 8000;
 
-app.listen(PORT, () => console.log(`server is running on Port :${PORT}`))
+const prisma = new PrismaClient();
+
+// 新規ユーザー登録API
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },j
+  });
+
+  return res.json({ user });
+});
+
+app.listen(PORT, () => console.log(`server is running on Port :${PORT}`));
