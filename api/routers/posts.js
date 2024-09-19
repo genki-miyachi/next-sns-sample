7184682edfb0e8d169f4e612712d6fecf6f32dea
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-19 15:47:13 +0900

    Add: post endpoint

---
+++ api/routers/posts.js
@@ -0,0 +1,56 @@
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+// つぶやき投稿API
+router.post("/post", async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    return res.status(400).json({ message: "投稿内容がありません" });
+  }
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        authorId: 1,
+      },
+    });
+
+    res.status(201).json({ newPost });  
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+
+  return res.json({ user });
+});
+
+// 最新つぶやき取得API
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   const user = await prisma.user.findUnique({ where: { email } });
+
+//   if (!user) {
+//     return res.status(400).json({ message: "User does not exists" });
+//   }
+
+//   const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//   if (!isPasswordValid) {
+//     return res.status(401).json({ message: "Invalid Password" });
+//   }
+
+//   const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
+//     expiresIn: "1d",
+//   });
+
+//   return res.json({ token });
+// });
+
+module.exports = router;
